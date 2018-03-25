03f811a9209ce8808d483dc6b4b8fa719a2286b6
kash <kashiwa.electory@gmail.com>
2018-03-25 22:58:44 +0900

    Update: main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,8 @@
 local g_time = "00:00"
+local receive_message_original = HUDChat.receive_message
 
 if RequiredScript == "lib/managers/hud/hudchat" then
-    function HUDChat:receive_message(name, message, color, icon)
+    function HUDChat:receive_message(name, ...)
         local heisttime = managers.game_play_central and managers.game_play_central:get_heist_timer() or 0
 		local hours = math.floor(heisttime / (60*60))
 		local minutes = math.floor(heisttime / 60) % 60
@@ -15,59 +16,6 @@ if RequiredScript == "lib/managers/hud/hudchat" then
 
         name = g_time .. " " .. name
 
-        local output_panel = self._panel:child("output_panel")
-        local len = utf8.len(name) + 1
-        local x = 0
-        local icon_bitmap = nil
-    
-        if icon then
-            local icon_texture, icon_texture_rect = tweak_data.hud_icons:get_icon_data(icon)
-            icon_bitmap = output_panel:bitmap({
-                y = 1,
-                texture = icon_texture,
-                texture_rect = icon_texture_rect,
-                color = color
-            })
-            x = icon_bitmap:right()
-        end
-    
-        local line = output_panel:text({
-            halign = "left",
-            vertical = "top",
-            hvertical = "top",
-            wrap = true,
-            align = "left",
-            blend_mode = "normal",
-            word_wrap = true,
-            y = 0,
-            layer = 0,
-            text = name .. ": " .. message,
-            font = tweak_data.menu.pd2_small_font,
-            font_size = tweak_data.menu.pd2_small_font_size,
-            x = x,
-            color = color
-        })
-        local total_len = utf8.len(line:text())
-    
-        line:set_range_color(0, len, color)
-        line:set_range_color(len, total_len, Color.white)
-    
-        local _, _, w, h = line:text_rect()
-    
-        line:set_h(h)
-        table.insert(self._lines, {
-            line,
-            icon_bitmap
-        })
-        line:set_kern(line:kern())
-        self:_layout_output_panel()
-    
-        if not self._focus then
-            local output_panel = self._panel:child("output_panel")
-    
-            output_panel:stop()
-            output_panel:animate(callback(self, self, "_animate_show_component"), output_panel:alpha())
-            output_panel:animate(callback(self, self, "_animate_fade_output"))
-        end
+        receive_message_original(self, name, ...)
     end
 end
